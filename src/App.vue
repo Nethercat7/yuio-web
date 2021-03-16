d9096de2ae9ec9b79270175b161df4a15c9417a2
Nethercat7 <nethercat7@foxmail.com>
2021-03-17 05:48:07 +0800

    引入vue router

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container style="min-height: 100vh">
+      <el-aside style="width:200px">Aside</el-aside>
+      <el-container>
+        <el-header> Header </el-header>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
+
+<style>
+body {
+  margin: 0;
+}
+</style>
