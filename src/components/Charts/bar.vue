06a5ce004eef0ea47d3a127f4c4abe4115277bee
Nethercat7 <nethercat7@foxmail.com>
2021-04-02 23:32:24 +0800

    就业率统计功能，和封装柱形图组件

---
+++ src/components/Charts/bar.vue
@@ -0,0 +1,114 @@
+<template>
+  <div :id="id" :style="{ width: width, height: height, margin: margin }"></div>
+</template>
+
+<script>
+import * as echarts from "echarts";
+export default {
+  props: {
+    id: {
+      type: String,
+      default: "main",
+    },
+    width: {
+      type: String,
+      default: "100%",
+    },
+    height: {
+      type: String,
+      default: "500px",
+    },
+    margin: {
+      type: String,
+      default: "auto",
+    },
+    showTooltip: {
+      type: Boolean,
+      default: true,
+    },
+    xData: {
+      type: Array,
+      default: null,
+    },
+    data: {
+      type: Array,
+      default: null,
+    },
+    showX: {
+      type: Boolean,
+      default: true,
+    },
+    percentage: {
+      type: Boolean,
+      default: false,
+    },
+    showTop: {
+      type: Boolean,
+      default: false,
+    },
+    showTitle: {
+      type: Boolean,
+      default: true,
+    },
+    title: {
+      type: String,
+      default: "Title",
+    },
+    subTitle: {
+      type: String,
+      default: null,
+    },
+  },
+  methods: {
+    inital() {
+      var chartDom = document.getElementById(this.id);
+      var myChart = echarts.init(chartDom);
+      var option;
+
+      option = {
+        title: {
+          show: this.showTitle,
+          text: this.title,
+          subtext: this.subTitle,
+          left: "center",
+        },
+        xAxis: {
+          show: this.showX,
+          type: "category",
+          data: this.xData,
+          axisLabel: {
+            interval: 0,
+          },
+        },
+        yAxis: {
+          type: "value",
+          axisLabel: {
+            show: true,
+            interval: "auto",
+            formatter: this.percentage ? "{value}%" : "{value}",
+          },
+        },
+        tooltip: {
+          show: this.showTooltip,
+          trigger: "axis",
+          formatter: this.percentage ? "{b}:{c}%" : "{b}:{c}",
+        },
+        series: [
+          {
+            data: this.data,
+            type: "bar",
+          },
+        ],
+      };
+
+      option && myChart.setOption(option);
+    },
+  },
+  mounted() {
+    this.inital();
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/statistics/employment/index.vue
+++ src/views/statistics/employment/index.vue
@@ -1,13 +1,89 @@
 <template>
-  <h1>就业率统计</h1>
+  <div>
+    <el-row class="card">
+      <el-col :span="12">
+        <EmploymentRate
+          :data="data"
+          :xData="xData"
+          :percentage="true"
+          title="各学院就业率"
+          subTitle="2021届"
+          :showX="true"
+          width="100%"
+        ></EmploymentRate>
+      </el-col>
+      <el-col :span="12">
+        <EmploymentRate
+          :data="data1"
+          :xData="xData"
+          title="各学院就业人数"
+          subTitle="2021届"
+          :showX="true"
+          width="100%"
+          id="cc"
+        ></EmploymentRate>
+      </el-col>
+    </el-row>
+    <el-row class="card">
+      <el-col :span="24">
+        <el-table :data="tableData">
+          <el-table-column
+            prop="name"
+            label="学院名称"
+          ></el-table-column>
+          <el-table-column
+            prop="total_people"
+            label="总人数"
+          ></el-table-column>
+          <el-table-column
+            prop="employment_people"
+            label="就业人数"
+          ></el-table-column>
+          <el-table-column
+            prop="unemployment_people"
+            label="未就业人数"
+          ></el-table-column>
+          <el-table-column
+            prop="employment_rate"
+            label="就业率"
+          ></el-table-column>
+          <el-table-column label="操作">
+            <el-button size="mini" type="info">查看详情</el-button>
+          </el-table-column>
+        </el-table>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
+import EmploymentRate from "@/components/Charts/bar";
 export default {
-
-}
+  components: { EmploymentRate },
+  data() {
+    return {
+      xData: [
+        "经济与管理学院",
+        "马克思学院",
+        "计算机学院",
+        "体育学院",
+        "艺术学院",
+        "汽车学院",
+        "电子信息工程学院",
+      ],
+      data: [90, 95, 89, 93, 86, 92, 97],
+      data1: [120, 200, 150, 80, 70, 110, 130],
+      tableData: [{
+        name:'经济与管理学院',
+        total_people:377,
+        employment_people:300,
+        unemployment_people:77,
+        employment_rate:'90%'
+      }]
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
