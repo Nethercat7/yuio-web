4fcfe35b0bbd241b0d87ae5788b15256d2e1df89
Nethercat7 <nethercat7@foxmail.com>
2021-04-06 22:41:44 +0800

    添加饼图组件

---
+++ src/components/Charts/pie.vue
@@ -0,0 +1,79 @@
+<template>
+  <div :id="id" :style="{ width: width, height: height, margin: margin }"></div>
+</template>
+
+<script>
+import * as echarts from "echarts";
+export default {
+  name: "PieChart",
+  props: {
+    id: {
+      type: String,
+      default: "main",
+    },
+    width: {
+      type: String,
+      default: "100%",
+    },
+    height: {
+      type: String,
+      default: "500px",
+    },
+    margin: {
+      type: String,
+      default: "auto",
+    },
+  },
+  methods: {
+    initial() {
+      var chartDom = document.getElementById(this.id);
+      var myChart = echarts.init(chartDom);
+      var option;
+
+      option = {
+        title: {
+          text: "某站点用户访问来源",
+          subtext: "纯属虚构",
+          left: "center",
+        },
+        tooltip: {
+          trigger: "item",
+        },
+        legend: {
+          orient: "vertical",
+          left: "left",
+        },
+        series: [
+          {
+            name: "访问来源",
+            type: "pie",
+            radius: "50%",
+            data: [
+              { value: 1048, name: "搜索引擎" },
+              { value: 735, name: "直接访问" },
+              { value: 580, name: "邮件营销" },
+              { value: 484, name: "联盟广告" },
+              { value: 300, name: "视频广告" },
+            ],
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: "rgba(0, 0, 0, 0.5)",
+              },
+            },
+          },
+        ],
+      };
+
+      option && myChart.setOption(option);
+    },
+  },
+  mounted() {
+    this.initial();
+  },
+};
+</script>
+
+<style>
+</style>
