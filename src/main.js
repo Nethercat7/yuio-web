d94580f41ac98230863c65ee7c38a469dcd52209
Nethercat7 <1209322690@qq.com>
2021-04-23 01:14:48 +0800

    添加新的工具类以及注册全局方法

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
+import { getDictData } from "@/api/system/dict/data"
+import { selectDictLabel } from "@/utils/ruoyi"
+
+//全局方法挂载
+Vue.prototype.getDictData = getDictData
+Vue.prototype.selectDictLabel = selectDictLabel
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
